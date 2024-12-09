645a089b2bbc9d5481a3446aea77372865be4acf
plestz <96513494+plestz@users.noreply.github.com>
2024-12-09 11:39:45 -0500

    feat: checkpoint, XGBClassifier SKF CV pipeline functions

--- src/pipeline_func.py
+++ src/pipeline_func.py
@@ -3,13 +3,14 @@ from sklearn.base import TransformerMixin
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import fbeta_score, make_scorer
-from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.model_selection import StratifiedKFold, train_test_split, ParameterGrid
+from xgboost import XGBClassifier
 
 def get_test_scores(X, y, preprocessor, model, param_grid, n_splits = 5, n_seeds = 5):
     """
     Given unpreprocessed full X and full y, runs an n_seeds number of Stratified K-Fold
     cross-validation hyperparameter tuning efforts, returning the results 
-    (best estimators, best estimators' test scores)of the best model from each seed.
+    (best estimators, best estimators' test scores) of the best model from each seed.
 
     Arguments:
         - X: unpreprocessed design matrix
@@ -44,7 +45,7 @@ def get_test_scores(X, y, preprocessor, model, param_grid, n_splits = 5, n_seeds
 
     return best_test_scores, best_estimators, best_test_scores[best_idx], best_estimators[best_idx]
 
-def stratified_kfold_cv_pipe(X, y, preprocessor: TransformerMixin, model, param_grid, n_splits: int = 5, random_state: int = 42):
+def stratified_kfold_cv_pipe(X_other, y_other, preprocessor: TransformerMixin, model, param_grid, n_splits: int = 5, random_state: int = 42):
     """
     Given unpreprocessed non-test X and non-test y, runs a Stratified K-Fold 
     Cross-Validation Grid Search on a pipeline with a preprocessor and model.
@@ -77,9 +78,105 @@ def stratified_kfold_cv_pipe(X, y, preprocessor: TransformerMixin, model, param_
     # (https://scikit-learn.org/dev/modules/generated/sklearn.model_selection.GridSearchCV.html)
     grid_search = GridSearchCV(pipeline, param_grid, scoring = f2_score, cv = cv, n_jobs = -1, verbose = 4)
 
-    grid_search.fit(X, y)
+    grid_search.fit(X_other, y_other)
 
     return grid_search.best_estimator_, grid_search.best_params_, grid_search.best_score_
+
+def get_xgb_classifier_test_scores(X, y, preprocessor: TransformerMixin, param_grid: dict, n_splits: int = 5, n_seeds: int = 5):
+    """
+    Given unpreprocessed full X and full y, runs an n_seeds number of Stratified K-Fold
+    cross-validation hyperparameter tuning efforts, returning the results 
+    (best estimators, best estimators' test scores) of the best model from each seed.
+
+    Note: This method should only be used for XGBClassifier models.
+
+    Arguments:
+        - X: unpreprocessed design matrix
+        - y: target variable
+        - preprocessor: a transformer, for data preprocessing in the pipeline
+        - param_grid: a dictionary, as the hyperparameters to grid search over
+        - n_splits: number of splits in the Stratified K-Fold
+        - n_seeds: number of seeds to run the Stratified K-Fold
+
+    Returns (in order):
+        - best_test_scores: list of test scores from the best model from each seed
+        - best_estimators: list of best estimators from each seed
+        - best_test_score: the best test score from all seeds
+        - best_estimator: the best estimator from all seeds
+    """
+    best_test_scores: list[float] = list()
+    best_estimators: list[Pipeline] = list()
+
+    for random_state in range(n_seeds):
+        print(f'Processing Seed {random_state + 1} of {n_seeds}...')
+        X_other, X_test, y_other, y_test = train_test_split(X, y, test_size = 0.2, random_state = random_state)
+        best_estimator, _, _ = xgb_classifier_cv(X_other, y_other, preprocessor, param_grid, n_splits, random_state)
+
+        best_test_score = test_pipe(X_test, y_test, best_estimator)
+
+        best_test_scores.append(best_test_score)
+        best_estimators.append(best_estimator)
+
+    best_idx = np.argmax(best_test_scores)
+
+    return best_test_scores, best_estimators, best_test_scores[best_idx], best_estimators[best_idx]
+
+def xgb_classifier_cv(X_other, y_other, preprocessor: TransformerMixin, param_grid: dict, n_splits: int = 5, random_state: int = 42):
+    """
+    Given unpreprocessed non-test X and non-test y, runs a Stratified K-Fold 
+    Cross-Validation Grid Search with a preprocessor and XGBClassifier.
+
+    Arguments:
+        - X: non-test, unpreprocessed design matrix
+        - y: non-test, target variable
+        - preprocessor: a transformer, for data preprocessing in the pipeline
+        - param_grid: a dictionary, as the hyperparameters to grid search over
+        - n_splits: number of splits in the Stratified K-Fold
+        - random_state: random state in the Stratified K-Fold
+
+    Returns:
+        - (best CV estimator, best CV params, best CV score) from grid search
+    """
+    best_model = None
+    best_params = None
+    best_score = float('-inf')
+
+    for params in ParameterGrid(param_grid):
+        print(f'Processing Parameter Combination {params}...')
+        model = XGBClassifier(**params, early_stopping_rounds = 10, n_jobs = -1, random_state = random_state)
+
+        pipeline = Pipeline(
+            steps = [
+                ('preprocessor', preprocessor),
+                ('model', model)
+            ]
+        )
+
+        skf = StratifiedKFold(n_splits = n_splits, shuffle = True, random_state = random_state)
+
+        cv_scores = list()
+        
+        for train_idx, val_idx in skf.split(X_other, y_other):
+            print(f'Processing SKF Split...')
+            X_train, y_train = X_other.iloc[train_idx], y_other.iloc[train_idx]
+            X_val, y_val = X_other.iloc[val_idx], y_other.iloc[val_idx]
+
+            pipeline.fit(X_train, y_train, model__eval_set = [(X_val, y_val)], model__verbose = False)
+
+            y_pred = pipeline.predict(X_val)
+
+            score = fbeta_score(y_val, y_pred, beta = 2)
+
+            cv_scores.append(score)
+
+        mean_score = np.mean(cv_scores)
+
+        if mean_score > best_score:
+            best_model = pipeline
+            best_params = params
+            best_score = mean_score
+
+    return best_model, best_params, best_score
 
 def test_pipe(X_test, y_test, pipeline: Pipeline):
     """
